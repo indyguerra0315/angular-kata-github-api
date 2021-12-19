98c73a86cb06b12623c8509cfce825a22464000c
Indira Guerra Rodríguez <iguerra@liddeo.com>
2021-12-19 12:07:54 +0100

    feat: Add home page

---
+++ src/app/components/search-input/search-input.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LoadingService } from '../../services/loading.service';
+
+@Component({
+  selector: 'app-search-input',
+  templateUrl: './search-input.component.html',
+  styleUrls: ['./search-input.component.css']
+})
+export class SearchInputComponent implements OnInit {
+
+  filter: string = '';
+  isLoading: boolean = false;
+  subscription: Subscription;
+
+  constructor(private router: Router,
+    private loadingService: LoadingService,
+    private route: ActivatedRoute
+  ) {
+    this.subscription = this.loadingService
+      .onToggle()
+      .subscribe((value) => (this.isLoading = value));
+  }
+
+  ngOnInit(): void {
+    this.filter = this.route.snapshot.paramMap.get('filter')!;
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe to ensure no memory leaks
+    this.subscription.unsubscribe();
+  }
+
+  search(event: string): void {
+    this.router.navigate(['search',  event ]);
+  }
+
+}

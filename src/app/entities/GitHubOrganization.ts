5fa7fe7352434284982954bbbed1a870ad3a874b
Indira Guerra Rodríguez <iguerra@liddeo.com>
2021-12-23 21:10:55 +0100

    feat: GitHub organization data

---
+++ src/app/entities/GitHubOrganization.ts
@@ -0,0 +1,6 @@
+export interface GitHubOrganization {
+  id: string,
+  login: string,
+  name?: string,
+  followers?: number
+}
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -5,6 +5,7 @@ import { map, catchError } from 'rxjs/operators';
 import { GitHubResponse } from '../entities/GitHubResponse';
 import { Item } from '../entities/Item';
 import { GitHubUser } from '../entities/GitHubUser';
+import { GitHubOrganization } from '../entities/GitHubOrganization';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,7 @@ export class GithubService {
   private apiUrl = 'https://api.github.com';
   private apiSearchUrl = '/search/users';
   private apiUserUrl = '/users';
+  private apiOrganizationUrl = '/orgs';
 
   constructor(private http: HttpClient) { }
 
@@ -23,6 +25,7 @@ export class GithubService {
     return this.http.get<any>(url)
       .pipe(
         switchMap((response: any) => {
+          // debugger;
 
             let getUsersData = response.items.map((user: any) => this.getUser(user));
 
@@ -51,18 +54,28 @@ export class GithubService {
     const url = `${this.apiUrl}${this.apiSearchUrl}?q=${filter} in:name type:org&per_page=4&page=${page}`;
     return this.http.get<any>(url)
       .pipe(
-        map((response: any) => {
-          let items: any[] = response.items;
+        switchMap((response: any) => {
 
-          let parsedItems = items.map(item => {
-            const newItem:Item = {id:item.id, code:item.login};
-            return newItem;
-          });
-          response.items = parsedItems;
+            let companiesData = response.items.map((organization: any) => this.getCompanyByLogin(organization.login));
 
-          return new GitHubResponse(response.total_count, response.items, page);
+            return forkJoin(companiesData)
+              .pipe(
+                map((itemsMerged: any) => {
+
+                  let parsedItems = itemsMerged.map(
+                    (item: any) => {
+                      const newItem:Item = {id:item.id, code:item.login, name:item.name, count:item.followers};
+                      return newItem;
+                    }
+                  );
+                  response.items = parsedItems;
+
+                  return new GitHubResponse(response.total_count, response.items, page);
+                })
+                // ,catchError(this.handleError)
+              );
         })
-        // , catchError(this.handleError)
+        // ,catchError(this.handleError)
       );
   }
 
@@ -71,17 +84,14 @@ export class GithubService {
     return this.http.get<GitHubUser>(url);
   }
 
-  // private handleError(error: HttpErrorResponse) {
-  //   if (error.status === 0) {
-  //     // A client-side or network error occurred. Handle it accordingly.
-  //     console.error('An error occurred:', error.error);
-  //   } else {
-  //     // The backend returned an unsuccessful response code.
-  //     // The response body may contain clues as to what went wrong.
-  //     console.error(
-  //       `Backend returned code ${error.status}, body was: `, error.error);
-  //   }
-  //   // Return an observable with a user-facing error message.
-  //   return throwError(() => new Error('Something bad happened; please try again later.'));
-  // }
+  getCompanyByLogin(companylogin: string): Observable<any> {
+    const url = `${this.apiUrl}${this.apiOrganizationUrl}/${companylogin}`;
+    return this.http.get<any>(url)
+      .pipe(
+        map((response: any) => {
+          const organizations:GitHubOrganization = {id:response.id, login:response.login, name:response.name, followers:response.followers};
+          return organizations;
+        })
+      );
+  }
 }
